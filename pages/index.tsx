235e6162b64552d795b9471e1acd900ee0a1a594
lxxonx <leeonechang92@gmail.com>
2022-06-26 20:17:05 +0900

    feat: 홈페이지 애니매이션 및 설명 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout';
+import Lottie from '@/components/Lottie';
 import siteMetadata from '@/data/siteMetadata';
 
 export const Home = (): JSX.Element => {
@@ -8,7 +9,33 @@ export const Home = (): JSX.Element => {
         title: `Home - ${siteMetadata.author}`,
       }}
     >
-      <h1>Home Page</h1>
+      <Lottie src="/animation/welcome.json"></Lottie>
+      <div className="text-center">
+        <div>본 웹사이트는 Next.js, Tailwind css, mdx 로 만들어졌습니다</div>
+        <div>현재 검색엔진 최적화를 위해 작업 중이며</div>
+        <div>
+          버그가 발생할 경우{' '}
+          <a
+            href="https://github.com/lxxonx/lxxonx.xyz/issues/new"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            여기
+          </a>
+          를 눌러 알려주시면 저에게 많은 도움이 됩니다
+        </div>
+        <div>
+          더 자세한 내용은{' '}
+          <a
+            href="https://github.com/lxxonx/lxxonx.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            깃허브링크
+          </a>
+          를 통해 확인하실 수 있습니다
+        </div>
+      </div>
     </Layout>
   );
 };
