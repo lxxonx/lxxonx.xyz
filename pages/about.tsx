cf8370e5f846ff41148d14460abc3606a007f746
lxxonx <leeonechang92@gmail.com>
2022-07-26 13:28:25 +0900

    fix: about responsivity 추가

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,10 +19,10 @@ export const About = (): JSX.Element => {
       <ShadowedBox className="bg-yellow-400 text-gray-700 font-extrabold mb-6">
         Who Am I?
       </ShadowedBox>
-      <div className="flex space-x-2">
+      <div className="flex space-y-3 md:space-y-0 md:space-x-2 flex-col md:flex-row">
         <OutlinedBox className="flex-1">
           <Carousel
-            className="mb-6"
+            className="mb-6 mx-auto"
             carouselItems={[
               <Image
                 key={1}
@@ -58,9 +58,20 @@ export const About = (): JSX.Element => {
               ></Image>,
             ]}
           ></Carousel>
-          <div>이름 이원창</div>
-          <div>생년월일 1992년 5월 3일</div>
-          <div>위치 대한민국 대전</div>
+          <div>
+            <div className="flex">
+              <div className="mr-2">이름: </div>
+              <strong className="text-xl">이원창</strong>
+            </div>
+            <div>생년월일 1992년 5월 3일</div>
+            <div>위치 대한민국 대전</div>
+          </div>
+
+          <div>
+            <h3>Websites</h3>
+            <hr />
+            <div>github: https://www.github.com/lxxonx</div>
+          </div>
         </OutlinedBox>
         <OutlinedBox className="flex-2">
           <h3 className="capitalize">bio</h3>
@@ -88,17 +99,32 @@ export const About = (): JSX.Element => {
         Who Can I Do?
       </ShadowedBox>
 
-      <div className="flex space-x-2">
+      <div className="flex space-y-3 md:space-y-0 md:space-x-2 flex-col md:flex-row">
         <OutlinedBox className="flex-1">
           <h3 className="capitalize">language</h3>
           <hr className="my-4" />
           <div>한국어 native</div>
           <div>영어 Advanced</div>
           <div>JS Can Create A Full Stack App</div>
+
+          <div>
+            JavaScript/TypeScript, HTML5, CSS3, Scss, Dart, Go, C#, Python
+            (Interested In: Rust, Elixir)
+          </div>
         </OutlinedBox>
         <OutlinedBox className="flex-1">
           <h3 className="capitalize">skill</h3>
           <hr className="my-4" />
+
+          <div>
+            - Frontend React.js, Vue.js, Nest.js, Styled Components, Tailwind
+            css, Apollo, Flutter (Interested In: Svelte, Electron, Storybook) -
+            Backend Nest.js, Express.js, Socket.io, Nginx, Prisma, TypeORM,
+            Mysql, Postgresql, Redis, RabbitMQ, Firebase (Interested In: NATS,
+            AWS DynamoDB, MongoDB, Planet Scale, Strapi, KeystoneJS) - DevOps
+            Docker, Docker Compose, Github, Github Actions, AWS ECS, AWS EC2
+            (Interested In: Kubernetes, Terraform)
+          </div>
           <div>구글링 Advanced</div>
           <div>커뮤니케이션 Advanced</div>
         </OutlinedBox>
