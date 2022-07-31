ef00c155b626e68eeade651fccb970104aadab72
lxxonx <leeonechang92@gmail.com>
2022-08-01 01:23:33 +0900

    chore: tailwind config modif && add tag on mdx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const { spacing } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
