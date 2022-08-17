b6b0790df07f545fefe6abc077a72b4ecc6ddc3d
lxxonx <leeonechang92@gmail.com>
2022-08-18 00:30:09 +0900

    fix: tag link bug fixed

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Tag = ({ text, type }: Props): JSX.Element => {
   return (
-    <Link href={`${type}/tags/${kebabCase(text)}`}>
+    <Link href={`/${type}/tags/${kebabCase(text)}`}>
       <a className="text-sm font-bold uppercase text-primary-400 hover:text-primary-500 dark:text-primary-300 dark:hover:text-primary-400 rounded-md bg-gray-100 dark:bg-gray-700 px-2 py-1">
         {text.split(' ').join('-')}
       </a>
